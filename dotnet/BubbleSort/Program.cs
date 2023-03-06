c7d17a71484dc1bcc9abf3d10b93d9211a845aae
ArtemDrozdov <drozdov_as@mail.ru>
2023-03-07 00:03:50 +0300

    сортировка пузырьком

--- dotnet/BubbleSort/Program.cs
+++ dotnet/BubbleSort/Program.cs
@@ -1,14 +1,14 @@
 ﻿using static Sorting;
 using static Infrastructure;
 
-// int[] array = {7, 6, 3, 4, 5, 1, 2, 3, 5, 6};
-
 // int[] array = CreateArray(10);
 // Show(array);
 // SortSelection(array);
 // Show(array);
 
-10.CreateArray(min:10, max:30)
-    .Show("|")
-    .SortSelection()
-    .Show("<=>");
+10.CreateArray()
+  .Show()
+//   .SortBubble()
+  .SortBubbleOptimized()
+  .Show();
+
