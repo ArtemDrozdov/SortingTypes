f7466c7b4b2c39abf82333ecc396fd457ec629cc
ArtemDrozdov <drozdov_as@mail.ru>
2023-03-06 13:14:26 +0300

    добавлен простой метод сортировки выбором

---
+++ dotnet/SelectionSort/Infrastructure.cs
@@ -0,0 +1,4 @@
+public static class Infrastructure
+{
+    public static int[] CreateArray(int size, int min = 0, int max = 10)
+}
---
+++ dotnet/SelectionSort/Program.cs
@@ -0,0 +1,25 @@
+﻿// using static Sorting;
+// using static Infrastructure;
+
+// // int[] array = CreateArray(10);
+// // Show(array);
+// // SortSelection(array);
+// // Show(array);
+
+// 10.CreateArray(min: 10, max: 5)
+//   .Show()
+//   .SortSelection()
+//   .Show();
+
+
+
+
+
+using static Sorting;
+using static System.Console;
+
+int[] array = {7, 6, 3, 4, 5, 1, 2, 3, 5, 6};
+
+WriteLine($"[{String.Join(',', array)}]");
+SortSelection(array);
+WriteLine($"[{String.Join(',', array)}]");
